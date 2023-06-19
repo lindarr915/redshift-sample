534513381df9c08cea13f8d74ece5cee94099a87
Darren Lin <lindarr@amazon.com>
2023-06-19 03:13:24 +0000

    add samples of async tasks

--- Program.cs
+++ Program.cs
@@ -1,45 +1,68 @@
 ﻿using System;
 using System.Data;
 using System.Data.Odbc;
+using System.IO;
+using System.Diagnostics;
 
 namespace redshift.amazon.com.docsamples
 {
     class ConnectToClusterExample
     {
+        static string? connString;
 
-        private static async Task ReadDataFromRedshiftAsync(int count)
+        private static async Task ReadDataFromRedshiftAsync(int count = 5)
         {
             // TODO: Complete the ExecuteNonQueryAsync()
-            // Task<bool>[] ReadDataTask = new Task<bool>[count];
+            Task<int>[] ReadDataTask = new Task<int>[count];
 
-            // for (int i = 0; i < count; i++)
-            // {
+            using (OdbcConnection connection = new OdbcConnection(connString))
+            {
+                connection.Open();
 
-            // }
-            // await Task.WhenAll(ReadDataTask);
+                // Perform your database operations here
+                for (int i = 0; i < count; i++)
+                {
+                    string query = "select 1";
+
+                    using (OdbcCommand command = new OdbcCommand(query, connection))
+                    {
+                        ReadDataTask[i] = command.ExecuteNonQueryAsync();
+                    }
+                }
+
+                await Task.WhenAll(ReadDataTask);
+
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine(String.Format("Task {0} completed. Result is {1}.", i, ReadDataTask[i].Result.ToString()));
+                }
+                connection.Close();
+            }
+
+
 
         }
-        
-        public static void Main(string[] args)
+
+        public static async Task Main(string[] args)
         {
 
             DataSet ds = new DataSet();
             DataTable dt = new DataTable();
 
             // Server, e.g. "examplecluster.xyz.us-west-2.redshift.amazonaws.com"
-            string server = System.Environment.GetEnvironmentVariable("DB_HOST");
+            string? server = System.Environment.GetEnvironmentVariable("DB_HOST");
 
             // Port, e.g. "5439"
-            string port = "5439";
+            string? port = "5439";
 
             // MasterUserName, e.g. "masteruser".
-            string masterUsername = System.Environment.GetEnvironmentVariable("DB_USER");
+            string? masterUsername = System.Environment.GetEnvironmentVariable("DB_USER");
 
             // MasterUserPassword, e.g. "mypassword".
-            string masterUserPassword = System.Environment.GetEnvironmentVariable("DB_PASSWORD");
+            string? masterUserPassword = System.Environment.GetEnvironmentVariable("DB_PASSWORD");
 
             // DBName, e.g. "dev"
-            string DBName = System.Environment.GetEnvironmentVariable("DB_NAME");
+            string? DBName = System.Environment.GetEnvironmentVariable("DB_NAME");
 
             // string query = "select * from information_schema.tables;";
             string query = "select 1";
@@ -48,16 +71,7 @@ namespace redshift.amazon.com.docsamples
             {
                 // Create the ODBC connection string.
                 //Redshift ODBC Driver - 64 bits
-                /*
-                string connString = "Driver={Amazon Redshift (x64)};" +
-                    String.Format("Server={0};Database={1};" +
-                    "UID={2};PWD={3};Port={4};SSL=true;Sslmode=Require",
-                    server, DBName, masterUsername,
-                    masterUserPassword, port);
-                */
-
-                //Redshift ODBC Driver - 32 bits
-                string connString = "Driver={Amazon Redshift (x64)};" +
+                connString = "Driver={Amazon Redshift (x64)};" +
                     String.Format("Server={0};Database={1};" +
                     "UID={2};PWD={3};Port={4};SSL=true;Sslmode=Require",
                     server, DBName, masterUsername,
@@ -84,9 +98,48 @@ namespace redshift.amazon.com.docsamples
                 conn.Close();
                 // Console.ReadKey();
 
-                // Thread.Sleep(100);
 
                 // TODO: Open a text file to run the SQL query 
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    Console.WriteLine("Iteration {0}", i);
+                    await ReadDataFromRedshiftAsync(5);
+                     stopwatch.Stop();
+                     TimeSpan duration = stopwatch.Elapsed;
+                     Console.WriteLine("Task duration: " + duration);
+
+                    await Task.Delay(100);
+                }
+
+                // Read args[0]
+                string fileName = args[0];
+
+                string filePath = "file.sql";
+
+                try
+                {
+                    // Read the entire file content as a string
+                    string fileContent = File.ReadAllText(filePath);
+                    Console.WriteLine(fileContent);
+
+                    // Alternatively, read the file content line by line
+                    // using a StreamReader
+                    // using (StreamReader reader = new StreamReader(filePath))
+                    // {
+                    //     string line;
+                    //     while ((line = reader.ReadLine()) != null)
+                    //     {
+                    //         Console.WriteLine(line);
+                    //     }
+                    // }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("An error occurred while reading the file: " + e.Message);
+                }
             }
             catch (Exception ex)
             {
